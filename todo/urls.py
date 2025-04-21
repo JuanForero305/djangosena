2532cfc01027f6904e2459460b827c3ce545998e
JuanForero305 <juandavidforerobarbosa@gmail.com>
2025-04-21 15:52:44 -0500

    proyecto django

--- todo/urls.py
+++ todo/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name="home"),
-    path("agregar/",views.agregar,name="agregar"),
+    path('',views.home,name="home"), 
+    path("agregar/", views.agregar, name="agregar"),
+    
 ]
